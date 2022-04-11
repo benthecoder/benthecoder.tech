acc3f74fb6db8c2d6db20844efe41a24b71f24cf
benthecoder <benthecoder07@gmail.com>
2022-04-11 11:01:20 -0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+  return <div className=' text-5xl font-bold underline'>Hello world!</div>;
+};
+
+export default Home;
